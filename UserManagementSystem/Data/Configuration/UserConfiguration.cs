37f2094dc8b5ec009a8d2addeab29973ee03d110
Viktor Stanev <viktorstanev192@gmail.com>
2025-11-19 16:31:10 +0200

    Configure User using Fluent API

---
+++ UserManagementSystem/Data/Configuration/UserConfiguration.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using UserManagementSystem.Data.Models;
+
+using static UserManagementSystem.Common.EntityValidationConstants.User;
+
+namespace UserManagementSystem.Data.Configuration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder
+                .HasKey(u => u.Id);
+
+            builder
+                .Property(u => u.Name)
+                .IsRequired()
+                .HasMaxLength(NameMaxLength);
+
+            builder
+                .Property(u => u.Username)
+                .IsRequired()
+                .HasMaxLength(UsernameMaxLength);
+
+            builder
+                .Property(u => u.Password)
+                .IsRequired()
+                .HasMaxLength(PasswordMaxLength);
+
+            builder
+                .Property(u => u.Email)
+                .IsRequired()
+                .HasMaxLength(EmailMaxLength);
+
+            builder
+                .Property(u => u.Phone)
+                .IsRequired()
+                .HasMaxLength(PhoneMaxLength);
+
+            builder
+                .Property(u => u.Website)
+                .IsRequired();
+
+            builder
+                .Property(u => u.Note);
+
+            builder
+                .Property(u => u.IsActive)
+                .IsRequired();
+
+            builder
+                .Property(u => u.CreatedAt)
+                .IsRequired();
+        }
+    }
+}
