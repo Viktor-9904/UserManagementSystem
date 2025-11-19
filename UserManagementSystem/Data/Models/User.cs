d0997364d3270fd9349db262898a700568f318a2
Viktor Stanev <viktorstanev192@gmail.com>
2025-11-19 15:53:05 +0200

    Create User Entity

---
+++ UserManagementSystem/Data/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace UserManagementSystem.Data.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string Website { get; set; } = null!;
+        public string? Note { get; set; }
+        public byte IsActive { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
