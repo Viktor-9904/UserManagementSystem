f425f2f47982a4c42a23d324a685081b70de7c95
Viktor Stanev <viktorstanev192@gmail.com>
2025-11-19 16:24:23 +0200

    Add User Entity Validation Constants

---
+++ UserManagementSystem/Common/EntityValidationConstants/User.cs
@@ -0,0 +1,11 @@
+﻿namespace UserManagementSystem.Common.EntityValidationConstants
+{
+    public static class User
+    {
+        public const int NameMaxLength = 100;
+        public const int UsernameMaxLength = 100;
+        public const int PasswordMaxLength = 200;
+        public const int EmailMaxLength = 200;
+        public const int PhoneMaxLength = 30;        
+    }
+}
