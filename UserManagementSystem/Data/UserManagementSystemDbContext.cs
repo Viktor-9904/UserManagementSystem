46cf984f59d77926c5e6ff761c7e26e2c41659a2
Viktor Stanev <viktorstanev192@gmail.com>
2025-11-19 17:52:42 +0200

    Add UserManagementSystemDbContext and initial EF Core migrations

---
+++ UserManagementSystem/Data/UserManagementSystemDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using UserManagementSystem.Data.Models;
+
+namespace UserManagementSystem.Data
+{
+    public class UserManagementSystemDbContext : DbContext
+    {
+        public UserManagementSystemDbContext(DbContextOptions<UserManagementSystemDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Address> Addresses { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
--- UserManagementSystem/Program.cs
+++ UserManagementSystem/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+using UserManagementSystem.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<UserManagementSystemDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
