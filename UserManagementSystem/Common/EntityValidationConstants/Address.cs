306eaee72060763005f3e1f4b69740bc442317f1
Viktor Stanev <viktorstanev192@gmail.com>
2025-11-19 16:26:20 +0200

    Add Address Entity Validation Constatnts

---
+++ UserManagementSystem/Common/EntityValidationConstants/Address.cs
@@ -0,0 +1,10 @@
+﻿namespace UserManagementSystem.Common.EntityValidationConstants
+{
+    public static class Address
+    {
+        public const int StreetMaxLength = 100;
+        public const int SuiteMaxLength = 100;
+        public const int CityMaxLength = 100;
+        public const int ZipcodeMaxLength = 10;
+    }
+}
