83dd96568661913e8020844cd280e0c6f6fd341e
Viktor Stanev <viktorstanev192@gmail.com>
2025-11-20 13:36:09 +0200

    Create Address ViewModel

---
+++ UserManagementSystem/ViewModels/AddressViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static UserManagementSystem.Common.EntityValidationConstants.Address;
+
+namespace UserManagementSystem.ViewModels
+{
+    public class AddressViewModel
+    {
+        [Required]
+        [MaxLength(StreetMaxLength)]
+        public string Street { get; set; } = null!;
+
+        [Required]
+        [MaxLength(SuiteMaxLength)]
+        public string Suite { get; set; } = null!;
+
+        [Required]
+        [MaxLength(CityMaxLength)]
+        public string City { get; set; } = null!;
+
+        [Required]
+        [MaxLength(ZipcodeMaxLength)]
+        public string Zipcode { get; set; } = null!;
+
+        [Required]
+        public double Lat { get; set; }
+
+        [Required]
+        public double Lng { get; set; }
+    }
+}
