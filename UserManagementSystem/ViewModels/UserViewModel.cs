ca0db9d2b35bcb15ff2e0a95c157e91be8c9383d
Viktor Stanev <viktorstanev192@gmail.com>
2025-11-20 13:42:13 +0200

    Create User ViewModel.

---
+++ UserManagementSystem/ViewModels/UserViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static UserManagementSystem.Common.EntityValidationConstants.User;
+
+namespace UserManagementSystem.ViewModels
+{
+    public class UserViewModel
+    {
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [MaxLength(UsernameMaxLength)]
+        public string Username { get; set; } = null!;
+
+        [Required]
+        [MaxLength(PasswordMaxLength)]
+        public string Password { get; set; } = null!;
+
+        [Required]
+        [MaxLength(EmailMaxLength)]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [MaxLength(PhoneMaxLength)]
+        public string Phone { get; set; } = null!;
+
+        [Required]
+        public string Website { get; set; } = null!;
+
+        public string? Note { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+
+        [Required]
+        public AddressViewModel Address { get; set; } = null!;
+    }
+}
