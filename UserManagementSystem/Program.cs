95e864bccd5528cb3dca86bcd7feaf82283b2ef3
Viktor Stanev <viktorstanev192@gmail.com>
2025-11-21 14:08:50 +0200

    Implement automatic migrations on startup

--- UserManagementSystem/Program.cs
+++ UserManagementSystem/Program.cs
@@ -19,6 +19,12 @@ builder.Services.AddScoped<IRepository, Repository>();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<UserManagementSystemDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
